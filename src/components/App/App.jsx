c0364d85da3b21696775b820e924ed7c5be5db93
Dmitry Kishkanev <150253586+DmitryKishkanev@users.noreply.github.com>
2025-09-20 18:03:25 +0300

    Добавил кнопку Close

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -151,6 +151,21 @@ class App extends Component {
         {showModal && (
           <Modal>
             <h1>Контент модалки</h1>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore
+              omnis rem quam numquam iusto ipsa, laudantium expedita quo fuga!
+              Doloribus ad accusantium atque ullam, accusamus esse ipsam tempore
+              odit soluta nulla dolorum quidem voluptatem et cumque excepturi a
+              incidunt cupiditate suscipit inventore nesciunt facere consectetur
+              repudiandae perferendis! Inventore, deleniti a.
+            </p>
+            <button
+              type="button"
+              className="Close__btn"
+              onClick={this.toggleModal}
+            >
+              Close
+            </button>
           </Modal>
         )}
       </div>
--- src/components/Modal/Modal.styled.js
+++ src/components/Modal/Modal.styled.js
@@ -10,7 +10,7 @@ const ModalContainer = styled.div`
   top: 0;
   left: 0;
   width: 100vw;
-  height: 100wh;
+  height: 100vh;
   background-color: rgba(0, 0, 0, 0.5);
 
   .Modal__content {
@@ -21,13 +21,21 @@ const ModalContainer = styled.div`
     min-height: 300px;
     max-width: 600px;
     width: 100%;
-    padding: 12px;
+    padding: 15px;
     background-color: #fff;
     border-radius: 3px;
     box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
       0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12);
+  }
 
-    color: black;
+  h1 {
+    margin-top: 0;
+
+    text-align: center;
+  }
+
+  .Close__btn {
+    margin-left: auto;
   }
 `;
 
