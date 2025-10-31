5b2b7fc9ea3a4b2301c414cf481fe2ac2884cb24
Dmitry Kishkanev <150253586+DmitryKishkanev@users.noreply.github.com>
2025-10-31 21:22:01 +0200

    проверка

--- src/components/News/APIfetchArticles/APIfetchArticles.js
+++ src/components/News/APIfetchArticles/APIfetchArticles.js
@@ -1,31 +1,31 @@
 import axios from 'axios';
 
-// const myKey = import.meta.env.VITE_TMDB_TOKEN;
+const myKey = import.meta.env.VITE_TMDB_TOKEN;
 
-// axios.defaults.headers.common['Authorization'] = `${myKey}`;
+axios.defaults.headers.common['Authorization'] = `${myKey}`;
 
-// const APIfetchArticles = ({
-//   searchQuery = '',
-//   currentPage = 1,
-//   pageSize = 5,
-// } = {}) => {
-//   return axios
-//     .get(
-//       `https://newsapi.org/v2/everything?q=${searchQuery}&pageSize=${pageSize}&page=${currentPage}`,
-//     )
-//     .then(response => response.data.articles);
-// };
-
-export const APIfetchArticles = ({
+const APIfetchArticles = ({
   searchQuery = '',
   currentPage = 1,
   pageSize = 5,
 } = {}) => {
   return axios
-    .get('/api/articles', {
-      params: { searchQuery, currentPage, pageSize },
-    })
-    .then(res => res.data);
+    .get(
+      `https://newsapi.org/v2/everything?q=${searchQuery}&pageSize=${pageSize}&page=${currentPage}`,
+    )
+    .then(response => response.data.articles);
 };
+
+// export const APIfetchArticles = ({
+//   searchQuery = '',
+//   currentPage = 1,
+//   pageSize = 5,
+// } = {}) => {
+//   return axios
+//     .get('/api/articles', {
+//       params: { searchQuery, currentPage, pageSize },
+//     })
+//     .then(res => res.data);
+// };
 
 export default APIfetchArticles;
