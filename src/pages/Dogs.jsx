d01ccd22b76e950ce5617ad8546310db9e7a933e
Dmitry Kishkanev <150253586+DmitryKishkanev@users.noreply.github.com>
2025-11-16 23:18:59 +0200

    добавил input и кнопку

--- src/pages/Dogs.jsx
+++ src/pages/Dogs.jsx
@@ -1,11 +1,27 @@
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  display: inline-flex;
+  flex-direction: column;
+`;
 
 const Dogs = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
   // useEffect(() => {
   //   // HTTP Запрос, если нужно
   // }, []);
   return (
-    <div>
+    <Container>
+      <input type="text" />
+      <button
+        onClick={() => {
+          setSearchParams();
+        }}
+      >
+        change sp
+      </button>
       {['dog-1', 'dog-2', 'dog-3', 'dog-4', 'dog-5'].map(dog => {
         return (
           <Link key={dog} to={`${dog}`}>
@@ -13,7 +29,7 @@ const Dogs = () => {
           </Link>
         );
       })}
-    </div>
+    </Container>
   );
 };
 
