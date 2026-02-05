4a820bd018f0f095b4b7302a6914112c38c8ba32
Dmitry Kishkanev <150253586+DmitryKishkanev@users.noreply.github.com>
2026-02-05 19:51:34 +0200

    оптимизировал  contactsSlice

--- src/redux/asyncReduxPhonebook/contactsSlice.js
+++ src/redux/asyncReduxPhonebook/contactsSlice.js
@@ -6,8 +6,42 @@ import {
   deleteContact,
 } from './phonebookOperations';
 
+const STATUS = {
+  PENDING: 'pending',
+  FULFILLED: 'fulfilled',
+  REJECTED: 'rejected',
+};
+
+const arrThunks = [fetchContacts, addContact, deleteContact];
+
+const oneOfTheThunks = type => arrThunks.map(el => el[type]);
+
 const handlePending = state => {
   state.isLoading = true;
+};
+
+const handleFulfilled = state => {
+  state.isLoading = false;
+  state.error = null;
+};
+
+const handleFulfilledFetch = (state, action) => {
+  state.items = action.payload;
+};
+
+const handleFulfilledAdd = (state, action) => {
+  // Добавляем контакт в конец списка
+  // state.items.push(action.payload);
+  // Добавляем контакт в начало списка
+  state.items.unshift(action.payload);
+  // Добавляем контакт в начало списка
+  // state.items = [action.payload, ...state.items];
+};
+
+const handleFulfilledDel = (state, action) => {
+  // const index = action.meta.arg;
+  const index = action.payload.id;
+  state.items = state.items.filter(contact => contact.id !== index);
 };
 
 // const handleRejected = (state, action) => {
@@ -31,48 +65,42 @@ export const asyncReduxPhonebookSlice = createSlice({
   initialState: phonebookInitialState.contacts,
 
   extraReducers: builder => {
+    const { PENDING, FULFILLED, REJECTED } = STATUS;
+
     builder
       // .addCase(fetchContacts.pending, handlePending)
-      .addCase(fetchContacts.fulfilled, (state, action) => {
-        state.isLoading = false;
-        state.error = null;
-        state.items = action.payload;
-      })
+      .addCase(fetchContacts.fulfilled, handleFulfilledFetch)
       // .addCase(fetchContacts.rejected, handleRejected)
       // .addCase(addContact.pending, handlePending)
-      .addCase(addContact.fulfilled, (state, action) => {
-        state.isLoading = false;
-        state.error = null;
-        // Добавляем контакт в конец списка
-        // state.items.push(action.payload);
-        // Добавляем контакт в начало списка
-        state.items.unshift(action.payload);
-        // Добавляем контакт в начало списка
-        // state.items = [action.payload, ...state.items];
-      })
+      .addCase(addContact.fulfilled, handleFulfilledAdd)
       // .addCase(addContact.rejected, handleRejected)
       // .addCase(deleteContact.pending, handlePending)
-      .addCase(deleteContact.fulfilled, (state, action) => {
-        state.isLoading = false;
-        state.error = null;
-        // const index = action.meta.arg;
-        const index = action.payload.id;
-        state.items = state.items.filter(contact => contact.id !== index);
-      })
+      .addCase(deleteContact.fulfilled, handleFulfilledDel)
       // .addCase(deleteContact.rejected, handleRejected)
       .addMatcher(
         isAnyOf(
-          fetchContacts.pending,
-          addContact.pending,
-          deleteContact.pending,
+          // fetchContacts.pending,
+          // addContact.pending,
+          // deleteContact.pending,
+          ...oneOfTheThunks(PENDING),
         ),
         handlePending,
       )
       .addMatcher(
         isAnyOf(
-          fetchContacts.rejected,
-          addContact.rejected,
-          deleteContact.rejected,
+          // fetchContacts.fulfilled,
+          // addContact.fulfilled,
+          // deleteContact.fulfilled,
+          ...oneOfTheThunks(FULFILLED),
+        ),
+        handleFulfilled,
+      )
+      .addMatcher(
+        isAnyOf(
+          // fetchContacts.rejected,
+          // addContact.rejected,
+          // deleteContact.rejected,
+          ...oneOfTheThunks(REJECTED),
         ),
         handleRejected,
       );
